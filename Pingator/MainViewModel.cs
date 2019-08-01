5fc591fa4fb1542b7011b5670da1364830925b27
vvvofka@yandex.ru <vvvofka@yandex.ru>
2019-08-01 14:05:06 +0300

    Добавьте файлы проекта.

---
+++ Pingator/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace Pingator {
+	class MainViewModel {
+		public ObservableCollection<Point> PointList { get; private set; }
+		public MainViewModel() {
+			PointList = new ObservableCollection<Point>();
+
+			// Some example data:
+			AddPoint(new Point(10, 10));
+			AddPoint(new Point(200, 200));
+			AddPoint(new Point(300, 300));
+		} // ///////////////////////////////////////////////////////////
+		public void AddPoint(Point p) {
+			// 3 at most, please!
+			PointList.Add(p);
+			if (PointList.Count == 3) {
+				PointList.RemoveAt(0);
+			}
+		} // ///////////////////////////////////////////////////////////////////
+		public void Del() {
+			if (PointList.Count > 0)
+				PointList.RemoveAt(0);
+		} // ////////////////////////////////////////////////////////////////////////
+	}
+}
---
+++ Pingator/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pingator {
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		MainViewModel mv;
+		public MainWindow() {
+			InitializeComponent();
+			mv = new MainViewModel();
+			this.DataContext = mv;
+			//this.DataContext = new MainViewModel();
+		} // //////////////////////////////////////////////////////////////////////////////
+
+		private void Btn1_Click(object sender, RoutedEventArgs e) {
+			mv.Del();
+		}
+	}
+}
