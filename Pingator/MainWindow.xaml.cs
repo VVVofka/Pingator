1d4f7ef7fbc53d5035e73391372ac9ebc3dd3d00
vvvofka@yandex.ru <vvvofka@yandex.ru>
2019-08-05 18:22:15 +0300

    dbger

--- Pingator/MainWindow.xaml.cs
+++ Pingator/MainWindow.xaml.cs
@@ -20,44 +20,23 @@ namespace Pingator {
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
-		private List<PingControl> pings = new List<PingControl>();
 		private List<PingControlAsync> pingas = new List<PingControlAsync>();
-		MainViewModel mv;
-		TimerPings tp;
-//		AutoResetEvent autoEvent = new AutoResetEvent(false);
-		int autoEvent = 0;
-		Timer stateTimer;
+		
 		public MainWindow() {
 			InitializeComponent();
-			mv = new MainViewModel();
-			this.DataContext = mv;
-			//this.DataContext = new MainViewModel();
-			stateTimer = new Timer(func, autoEvent, 3000, 7000000);
 			PingSimplInit();
-			tp = new TimerPings(7000, pingas);
+			DataContext = new TimerPings(7000, pingas);
 		} // //////////////////////////////////////////////////////////////////////////////
-		public void func(Object i) {
-			Btn1_Click(btn1, null);
-		} // ///////////////////////////////////////////////////////////////////////////////
 		private void Btn1_Click(object sender, RoutedEventArgs e) {
 			//Thread.Sleep(7000);
 			Console.WriteLine("complete");
-			tp.Cycle();
 		} // /////////////////////////////////////////////////////////////////////////////////
 		private void PingSimplInit() {
-			pings.Add(new PingControl("192.168.1.1", rtSecr));
-			pings.Add(new PingControl("192.168.1.199", swSecr));
-			pings.Add(new PingControl("google.com", pcSecr));
-			pings.Add(new PingControl("192.168.2.199", pcDirect));
 			pingas.Add(new PingControlAsync("192.168.1.1", rtSecr));
 			pingas.Add(new PingControlAsync("192.168.1.199", swSecr));
 			pingas.Add(new PingControlAsync("google.com", pcSecr));
 			pingas.Add(new PingControlAsync("192.168.2.199", pcDirect));
 			pingas.Add(new PingControlAsync("192.168.1.198", pcResurs));
 		} // //////////////////////////////////////////////////////////////////////////////////
-		private void Window_Closed(object sender, EventArgs e) {
-			//this.RaiseEvent()
-			//Invalidate
-		} // /////////////////////////////////////////////////////////////////////////////////////////
 	} // -------------------------------------------------------------------------------------
 }
